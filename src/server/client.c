576c3bd365ec169055e66e2b3d17dc5dcb671583
Yehor <hawoord@gmail.com>
2025-09-16 22:03:58 +0300

    simple client client-server communication that allows sending one message from client and server. Now server accepts one connection - receives a single message - sends a reply, then exists

---
+++ src/server/client.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <stdlib.h>
+#define PORT 8080
+//use getaddrinfo for modern, cross platform hostname resolution
+
+int __cdecl main() {
+    WSADATA wsaData;
+    //Initialize Winsock
+    int err = WSAStartup(MAKEWORD(2,2), &wsaData); //Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h 
+    if (err != 0) {
+        printf("WSAStartup failed with error: %d\n", err);  //Tell the user that we could not find a usable Winsock DLL
+        //WSAStart up directly returns the extended error code in the return value for this function. 
+        return 1;   
+    }
+
+    //Create socket
+    SOCKET sock;
+    sock = socket(AF_INET, SOCK_STREAM, 0); // AF_INET - IPV4, SOCK_Stream - TCP protocol
+    if(sock == INVALID_SOCKET) {
+        printf("Could not create a socket: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    //Server details
+    struct sockaddr_in server;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT); //Default Redis port - 6379, change it later
+    
+    //Conect to server
+    if(connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Connection failed with error code: %d\n", WSAGetLastError());
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Connected to a server! \n");
+    // The Winsock DLL is acceptable. Proceed to use it. 
+    //const char *msg = "PING\r\n";
+    //send(sock, msg, strlen(msg), 0);
+
+        //Send a response to a client
+    char mesg[200];
+    printf("Enter the message (reply): ");
+    fgets(mesg, sizeof(mesg),stdin);
+    int sbyteCount = send(sock, mesg, 200, 0);
+    if (sbyteCount == SOCKET_ERROR) {
+        printf("Client send error: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    //Recive reply
+    char buffer[1024] = {0};
+    int bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    if (bytes > 0 ) {
+        buffer[bytes] = '\0';
+        printf("Reply: %s", buffer);
+    }
+    else {
+        printf("No response \n");
+    }
+
+    // then call WSACleanup when done using the Winsock dll 
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
--- src/server/server.c
+++ src/server/server.c
@@ -3,43 +3,83 @@
 #include <ws2tcpip.h>
 #include <windows.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #define PORT 8080
-//use getaddrinfo for modern, cross platform hostname resolution
 
-int __cdecl main() {
+int main() {
     WSADATA wsaData;
-    //Initialize Winsock
-    int err = WSAStartup(MAKEWORD(2,2), &wsaData); //Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h 
+
+    int err = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (err != 0) {
-        printf("WSAStartup failed with error: %d\n", err);  //Tell the user that we could not find a usable Winsock DLL
-        //WSAStart up directly returns the extended error code in the return value for this function. 
-        return 1;   
-    }
-
-    //Create socket
-    SOCKET sock;
-    sock = socket(AF_INET, SOCK_STREAM, 0); // AF_INET - IPV4, SOCK_Stream - TCP protocol
-    if(sock == INVALID_SOCKET) {
-        printf("Could not create a socket: %d\n", WSAGetLastError());
-    }
-
-    //Server details
-    struct sockaddr_in server;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8080); //Default Redis port - 6379, change it later
-    
-    //Conect to server
-    if(connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        printf("Connection failed with error code: %d\n", WSAGetLastError());
+        printf("WSAStartup failed with error: %d\n", err);
         return 1;
     }
 
-    printf("Connected to a server! \n");
-    // The Winsock DLL is acceptable. Proceed to use it. 
-    // then call WSACleanup when done using the Winsock dll 
-    closesocket(sock);
+    //Create a socket
+    SOCKET serverSocket;
+    serverSocket = INVALID_SOCKET;
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        printf("Error at socket: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    //Bind the Socket
+    struct sockaddr_in service;
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = inet_addr("127.0.0.1");
+    service.sin_port = htons(PORT);
+
+    //Use the bind function
+    if (bind(serverSocket,(struct sockaddr *) &service, sizeof(service)) == SOCKET_ERROR) {
+        printf("Bind failed: %d\n", WSAGetLastError());
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    //Listen for incoming connections
+    if (listen(serverSocket, 1) == SOCKET_ERROR) {
+        printf("Error listening on socket: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    //Accept incoming connections
+    SOCKET acceptSocket;
+    acceptSocket = accept(serverSocket, NULL, NULL);
+
+    //Check for succesfull connections
+    if (acceptSocket == INVALID_SOCKET) {
+        printf("Accept failed: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    //Recive data from the client
+    char receiveBuffer[200];
+    int rbyteCount = recv(acceptSocket,receiveBuffer, 200, 0);
+    if (rbyteCount == SOCKET_ERROR) {
+        printf("Server recive error: %d\n", WSAGetLastError());
+        return 1;
+    }
+    if (rbyteCount > 0) {
+        receiveBuffer[rbyteCount] = '\0';
+        printf("Message from a client: %s", receiveBuffer);
+    }
+
+    //Send a response to a client
+    char buffer[200];
+    printf("Enter the message (reply): ");
+    fgets(buffer, sizeof(buffer),stdin);
+    int sbyteCount = send(acceptSocket, buffer, 200, 0);
+    if (sbyteCount == SOCKET_ERROR) {
+        printf("Server send error: %d\n", WSAGetLastError());
+        return 1;
+    }
+    closesocket(acceptSocket);
+    closesocket(serverSocket);
     WSACleanup();
+
     return 0;
 }
